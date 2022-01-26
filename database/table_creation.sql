1a8a76203abb9d9ba79bfa504ed23132160d8650
Maciej Chylak <56119413+maciekchylak@users.noreply.github.com>
2022-01-26 19:37:08 +0100

    Added displayed_name column

--- database/table_creation.sql
+++ database/table_creation.sql
@@ -3,24 +3,26 @@ USE y7snb0jclmyepnar;
 CREATE TABLE Products(
 	product_id INTEGER NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(50) NOT NULL,
-    category VARCHAR(50) NOT NULL,
+   	category VARCHAR(50) NOT NULL,
 	quantity FLOAT(10, 1) NULL,
 	unit VARCHAR(50) NULL,
-    price FLOAT(10,2) NOT NULL,
-    PRIMARY KEY (product_id)
+   	price FLOAT(10,2) NOT NULL,
+   	displayed_name VARCHAR(50) NULL,
+   	PRIMARY KEY (product_id)
 );
+
 
 CREATE TABLE Orders(
 	order_id INTEGER NOT NULL AUTO_INCREMENT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NULL,
-    zip_code VARCHAR(50) NULL,
-    street VARCHAR(50) NULL,
-    building_number VARCHAR(10) NULL,
-    flat_number INTEGER NULL,
+ 	last_name VARCHAR(50) NOT NULL,
+    	city VARCHAR(50) NULL,
+    	zip_code VARCHAR(50) NULL,
+    	street VARCHAR(50) NULL,
+    	building_number VARCHAR(10) NULL,
+    	flat_number INTEGER NULL,
 	price FLOAT(30, 2) NOT NULL,
-    items JSON NOT NULL,
-    PRIMARY KEY (order_id)
+    	items JSON NOT NULL,
+    	PRIMARY KEY (order_id)
 );
